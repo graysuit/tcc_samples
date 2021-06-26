b7ca3a96249a006070070d1fd15707ede61c3428
Gray Programmerz <hackrefisher@gmail.com>
2021-06-26 15:42:30 +0500

    Add files via upload

---
+++ samples/sum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>	// atoi()
+
+int sum(a , b)
+{
+	return a + b;
+}
+
+int main(int argc, char **argv) 
+{
+	if (argc < 3) {
+		printf("usage: sum.exe num1 num2\n"
+			   "example: sum.exe 2 4\n");
+		return 1;
+	}
+		
+	int a = atoi(argv[1]);
+	int b = atoi(argv[2]);
+	printf("%d + %d = %d\n", a, b, sum(a , b));
+	return 0;
+}
