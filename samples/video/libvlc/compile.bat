990e9fd30935f1c566904a0174cdc424a81d4859
Gray Programmerz <hackrefisher@gmail.com>
2021-06-29 17:52:10 +0500

    Update compile.bat

--- samples/video/libvlc/compile.bat
+++ samples/video/libvlc/compile.bat
@@ -1,4 +1,5 @@
-bitsadmin.exe /transfer myDownloadJob /download /priority normal http://globalcdn.nuget.org/packages/videolan.libvlc.windows.3.0.14.nupkg libvlc.nupkg
+REM bitsadmin.exe /transfer myDownloadJob /download /priority normal http://globalcdn.nuget.org/packages/videolan.libvlc.windows.3.0.14.nupkg 
+powershell.exe (New-Object System.Net.WebClient).DownloadFile("http://globalcdn.nuget.org/packages/videolan.libvlc.windows.3.0.14.nupkg", "libvlc.nupkg")
 %zip% x libvlc.nupkg build\x86\* -obin
 xcopy %CD%\bin\build\x86 %CD%\bin /S /E /Y
 del bin\build\* /s /f /q
