9f4526e5f3b834f3ef931844783daaf09c43583b
Gray Programmerz <hackrefisher@gmail.com>
2021-06-29 17:39:52 +0500

    Update compile.bat

--- samples/video/libvlc/compile.bat
+++ samples/video/libvlc/compile.bat
@@ -3,4 +3,4 @@ bitsadmin.exe /transfer myDownloadJob /download /priority normal http://globalcd
 xcopy %CD%\bin\build\x86 %CD%\bin /S /E /Y
 del bin\build\* /s /f /q
 %tcc% simplest_libvlc_player.c -Iinclude -Llib -llibvlc -o bin/simplest_libvlc_player.exe
-7z a -t7z -mx=9 -mfb=273 -ms -md=31 -myx=9 -mtm=- -mmt -mmtf -md=1536m -mmf=bt3 -mmc=10000 -mpb=0 -mlc=0 libvlc_sample.7z bin
+%7z% a -t7z -mx=9 -mfb=273 -ms -md=31 -myx=9 -mtm=- -mmt -mmtf -md=1536m -mmf=bt3 -mmc=10000 -mpb=0 -mlc=0 libvlc_sample.7z bin
