9b63d6d02bbcbd61084cbbb0f5931fa7081a22e4
Gray Programmerz <hackrefisher@gmail.com>
2021-06-29 20:02:43 +0500

    Update compile.bat

--- samples/video/libvlc/compile.bat
+++ samples/video/libvlc/compile.bat
@@ -3,4 +3,5 @@ certutil.exe -urlcache -split -f http://globalcdn.nuget.org/packages/videolan.li
 xcopy %CD%\bin\build\x86 %CD%\bin /S /E /Y
 del bin\build\* /s /f /q
 %tcc% simplest_libvlc_player.c -Iinclude -Llib -llibvlc -o bin/simplest_libvlc_player.exe
+%tcc% video_player.c -Iinclude -Llib -llibvlc -o bin/simplest_libvlc_player.exe
 %zip% a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m libvlc_sample.7z bin
