a219e6573849dbe4c75163b9dcd35056e52aa68d
Gray Programmerz <hackrefisher@gmail.com>
2021-06-29 18:14:50 +0500

    Update compile.bat

--- samples/video/libvlc/compile.bat
+++ samples/video/libvlc/compile.bat
@@ -1,8 +1,6 @@
-REM bitsadmin.exe /transfer myDownloadJob /download /priority normal http://globalcdn.nuget.org/packages/videolan.libvlc.windows.3.0.14.nupkg 
 certutil.exe -urlcache -split -f http://globalcdn.nuget.org/packages/videolan.libvlc.windows.3.0.14.nupkg libvlc.nupkg
 %zip% x libvlc.nupkg build\x86\* -obin
 xcopy %CD%\bin\build\x86 %CD%\bin /S /E /Y
 del bin\build\* /s /f /q
 %tcc% simplest_libvlc_player.c -Iinclude -Llib -llibvlc -o bin/simplest_libvlc_player.exe
-%zip% a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on libvlc_sample.7z bin
-dir
+%zip% a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m libvlc_sample.7z bin
